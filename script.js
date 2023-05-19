704fe0ca6e7f960ed2d27a531e9e29b5fde91fc7
Nikoloz-Shamugia <129981552+Nikoloz-Shamugia@users.noreply.github.com>
2023-05-19 17:12:23 +0400

    .

---
+++ script.js
@@ -0,0 +1,26 @@
+    //   variables 
+let age = 30;
+let child1Age = 12;
+let child1Name = "nino";
+let child2Name = "mamuka";
+let childAge2 = 8 ;
+let name = "Niko";
+let lastName = "shamugia";
+let married = false;
+let nothing = "";
+let car = null;
+    // operations
+console.log( child1Age + childAge2);
+console.log( child1Age - childAge2);
+console.log( child1Age * childAge2);
+console.log( child1Age / childAge2);
+console.log( child1Age + name);
+console.log(nothing * car);
+
+    // sentences
+console.log("my name is " + name + " " + lastName + " i am " + age + " years old and i have two children " + child1Name + " and " + child2Name  + " . " + child1Name + " is " + child1Age + " years old and " + child2Name + " is " + childAge2 + " years old" )
+console.log(`my name is ${name} ${lastName} i am ${age} years old and i have two children ${child1Name} and ${child2Name} . ${child1Name} is ${child1Age} years old and ${child2Name} is ${childAge2} years old `)
+    // rectangle
+let width = 5 ;
+let lenght = 7 ;
+console.log( width * lenght)
